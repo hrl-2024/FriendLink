b76497193ead02c93d2a677ba13e7c0044f64035
hrl-2024 <hrl@bu.edu>
2023-11-14 20:34:26 -0500

    FriendsFragment refactor done

--- app/src/main/java/com/skr/android/friendlink/ui/dashboard/FriendsFragment.kt
+++ app/src/main/java/com/skr/android/friendlink/ui/dashboard/FriendsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.skr.android.friendlink.databinding.FragmentDashboardBinding
+import com.skr.android.friendlink.databinding.FragmentFriendsBinding
 
-class DashboardFragment : Fragment() {
+class FriendsFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFriendsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val friendsViewModel =
+            ViewModelProvider(this).get(FriendsViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFriendsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textFriends
+        friendsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
