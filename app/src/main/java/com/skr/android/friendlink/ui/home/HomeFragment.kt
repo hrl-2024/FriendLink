2c11e3e94442e6933f112150002f276cdb4d51ed
hrl-2024 <hrl@bu.edu>
2023-11-28 22:48:19 -0500

    UI theme color change + Message sending page UI

--- app/src/main/java/com/skr/android/friendlink/ui/home/HomeFragment.kt
+++ app/src/main/java/com/skr/android/friendlink/ui/home/HomeFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.skr.android.friendlink.databinding.FragmentHomeBinding
+import com.skr.android.friendlink.databinding.FragmentHomeLaunchBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentHomeLaunchBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +25,9 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeLaunchBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
         return root
     }
 
