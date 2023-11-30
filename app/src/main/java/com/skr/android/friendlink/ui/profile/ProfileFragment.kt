d6f160598485df054ca4fd3586b65f848ac27edb
Saisriram Gunturu <saisriram@saimail.com>
2023-11-29 19:12:15 -0500

    Updated profile page

--- app/src/main/java/com/skr/android/friendlink/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/skr/android/friendlink/ui/profile/ProfileFragment.kt
@@ -7,33 +7,51 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.skr.android.friendlink.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.profile
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        firebaseAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        // Get current user
+        val currentUser = firebaseAuth.currentUser
+        val userId = currentUser?.uid
+
+        val userDocRef = userId?.let { firestore.collection("users").document(it) }
+
+        userDocRef?.get()?.addOnSuccessListener { document ->
+            if (document != null && document.exists()) {
+                val firstName = document.getString("first")
+                val lastName = document.getString("last")
+                val email = currentUser.email
+
+                // Update UI with retrieved user data
+                binding.firstName.text = firstName
+                binding.lastName.text = lastName
+                binding.email.text = email
+            }
         }
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
