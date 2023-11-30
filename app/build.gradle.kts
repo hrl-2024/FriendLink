ebe624bc15cd0ad727a49e3547b24b9f369c26f8
Saisriram Gunturu <saisriram@saimail.com>
2023-11-29 20:45:39 -0500

    Allow user to upload profile picture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,9 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-firestore:24.9.1")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
