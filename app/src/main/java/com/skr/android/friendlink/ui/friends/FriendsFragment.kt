42f9390ce7121fbb6bf3990955e2923e2ed695cc
hrl-2024 <hrl@bu.edu>
2023-12-02 23:18:21 -0500

    Read from contact

--- app/src/main/java/com/skr/android/friendlink/ui/friends/FriendsFragment.kt
+++ app/src/main/java/com/skr/android/friendlink/ui/friends/FriendsFragment.kt
@@ -1,9 +1,16 @@
 package com.skr.android.friendlink.ui.friends
 
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -13,6 +20,8 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.skr.android.friendlink.databinding.FragmentFriendsBinding
 import kotlinx.coroutines.launch
+
+private const val TAG = "FriendsFragment"
 
 class FriendsFragment : Fragment() {
 
@@ -42,16 +51,75 @@ class FriendsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                friendsViewModel.friends.collect { friends ->
-                    binding.friendRecyclerView.adapter = FriendListAdapter(friends)
-                }
-            }
+
+            val friendsList = getFriends()
+            Log.d(TAG, "Got ${friendsList.size} phone numbers from contacts")
+            binding.friendRecyclerView.adapter = FriendListAdapter(friendsList)
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    @SuppressLint("Range")
+    fun getFriends() : List<Friend> {
+
+        // check permission
+        if (ContextCompat.checkSelfPermission(requireActivity(),
+                android.Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.READ_CONTACTS),
+                1
+            )
+        }
+
+        val friendsList = mutableListOf<Friend>()
+
+        val contentResolver = requireActivity().contentResolver
+        val cursor = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        if (cursor!!.count > 0) {
+            while (cursor.moveToNext()) {
+                val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                val phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)).toInt()
+
+                if (phoneNumber <= 0) {
+                    continue
+                } else {
+                    val cursorPhone = contentResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?",
+                        arrayOf(id),
+                        null
+                    )
+                    if (cursorPhone!!.count > 0) {
+                        while (cursorPhone.moveToNext()) {
+                            val phoneNumValue = cursorPhone.getString(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                            Log.d(TAG, "Name: $name")
+                            Log.d(TAG, "Phone Number: $phoneNumValue")
+                            friendsList.add(Friend(id, "", name, "", phoneNumValue.toString()))
+                        }
+                    }
+                    cursorPhone.close()
+                }
+            }
+        }
+
+        cursor.close()
+
+        return friendsList
     }
 }
