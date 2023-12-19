49fd38398ee6f8280b52dd0576a6b9064c001762
Saisriram Gunturu <saisriram@saimail.com>
2023-12-19 16:01:25 -0500

    Message Feed only shows messages recieved today

--- app/src/main/java/com/skr/android/friendlink/ui/home/feed/MessageListViewModel.kt
+++ app/src/main/java/com/skr/android/friendlink/ui/home/feed/MessageListViewModel.kt
@@ -9,6 +9,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.util.Calendar
 
 class MessageListViewModel : ViewModel() {
     // Replace MutableLiveData with MutableStateFlow for messages
@@ -53,6 +54,76 @@ class MessageListViewModel : ViewModel() {
                                     timestamp = document.getLong("timestamp") ?: 0L
                                 )
                                 messages.add(message)
+                            }
+                            if (messages.isNotEmpty()) {
+                                _messages.value = messages
+                            } else {
+                                // No messages found
+                                _messages.value = emptyList()
+                            }
+                        }
+                        .addOnFailureListener { e ->
+                            Log.d("MessageListViewModel", "Error getting documents: ", e)
+                            // Handle failure in fetching messages from Firestore
+                        }
+                } else {
+                    // No messages found
+                    _messages.value = emptyList()
+                }
+            }
+        }
+    }
+    fun fetchTodayMessages(forSent: Boolean = false) {
+        val firestore = FirebaseFirestore.getInstance()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val userId = currentUser?.uid
+
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        val startOfDay = calendar.timeInMillis // Start of the current day
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        calendar.add(Calendar.MILLISECOND, -1)
+
+        val endOfDay = calendar.timeInMillis
+
+        val userDocRef = userId?.let { firestore.collection("users").document(it) }
+
+        userDocRef?.get()?.addOnSuccessListener { documentSnapshot ->
+            if (documentSnapshot != null && documentSnapshot.exists()) {
+
+                var messageList = emptyList<String>()
+
+                if (forSent) {
+                    messageList = documentSnapshot.get("sentList") as? List<String> ?: emptyList()
+                } else {
+                    messageList = documentSnapshot.get("receivedList") as? List<String> ?: emptyList()
+                }
+
+                if (messageList.isNotEmpty()) {
+                    firestore.collection("messages")
+                        .whereIn(FieldPath.documentId(), messageList)
+                        .get()
+                        .addOnSuccessListener { querySnapshot ->
+                            val messages = mutableListOf<Message>()
+
+                            for (document in querySnapshot.documents) {
+                                val message = Message(
+                                    sender = document.getString("sender") ?: "",
+                                    receiver = document.getString("receiver") ?: "",
+                                    message = document.getString("message") ?: "",
+                                    timestamp = document.getLong("timestamp") ?: 0L
+                                )
+                                // Filter messages by timestamp
+                                if (message.timestamp in startOfDay..endOfDay) {
+                                    messages.add(message)
+                                }
                             }
                             if (messages.isNotEmpty()) {
                                 _messages.value = messages
