82a55b52b1fb3ac782adad1b76ba78193f0c5c69
Kristi Li <kli1226@bu.edu>
2023-12-19 10:39:12 -0500

    Added 2 files for loading after sending a message; not working yet

---
+++ app/src/main/java/com/skr/android/friendlink/ui/home/send/LoadingFragment.kt
@@ -0,0 +1,40 @@
+package com.skr.android.friendlink.ui.home.send
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.skr.android.friendlink.R
+import com.skr.android.friendlink.databinding.FragmentSentBinding
+import android.os.Handler
+import android.os.Looper
+
+class LoadingFragment : Fragment() {
+
+    private var _binding: FragmentSentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Navigate to the 'Message Sent' page after 3 seconds
+            findNavController().navigate(R.id.action_send_to_sent)
+        }, 3000)
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
